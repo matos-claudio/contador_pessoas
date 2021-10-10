715a4c409efcd215a2f7df13751634d172e78f6c
Claudio Matos <cloud.devmob@gmail.com>
2021-10-10 01:11:09 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: 'Contador de Pessoas', home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "Pode entrar!!!";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+      _people > 0
+          ? _infoText = "Pode entrar!!!"
+          : _infoText = "Mundo invertido!";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpeg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: OutlinedButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: Text(
+                        "+ 1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: OutlinedButton(
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                      child: Text(
+                        "- 1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      )),
+                )
+              ],
+            ),
+            Text(
+              _infoText,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
